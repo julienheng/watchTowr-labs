4fa27e05aab13bff8ff8800d639476bcda14ec52
julienheng <hengjchan@gmail.com>
2024-01-05 00:55:09 +0800

    writing sample dataset

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return <div>App</div>;
+  const [data, dataSet] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/hunts')
+      .then((response) => response.json())
+      .then((data) => dataSet(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className="">
+      <h1>Hunts</h1>
+      <ul>
+        {data.map((hunt: any) => (
+          <li key={hunt.id}>{hunt.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
