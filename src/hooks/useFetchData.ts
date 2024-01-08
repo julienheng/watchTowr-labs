b2041a8313269ee5dfc900e5df8f24a81622628d
julienheng <hengjchan@gmail.com>
2024-01-08 21:41:06 +0800

    refactoring

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,23 +1,8 @@
 import { useQuery } from 'react-query';
-import { HuntType } from '@/types/HuntType';
 import { AssetType } from '@/types/AssetType';
 import { TenantType } from '@/types/TenantType';
 import { useThreatStore } from '@/stores/threatStateStore';
 
-export const useFetchData = () => {
-  const { data, isLoading, error } = useQuery<HuntType[]>(
-    'tenants',
-    async () => {
-      const response = await fetch('http://localhost:3000/tenants');
-      const apiData = await response.json();
-      return apiData;
-    },
-  );
-
-  // console.log(data);
-
-  return { data, isLoading, error };
-};
 
 export const useAssetData = () => {
   const setThreatValue = useThreatStore((state) => state.setThreatValue);
@@ -33,7 +18,6 @@ export const useAssetData = () => {
 
   // Find the number of affected assets
   const affectedNum = data?.filter((item) => item.status === 'vulnerable')?.length;
-
   setThreatValue(affectedNum || 0);
 
 
